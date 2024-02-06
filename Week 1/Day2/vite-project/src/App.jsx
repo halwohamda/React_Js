9b8ecdaeaa96a70d613faabd31ac53c267397825
halwohamda <halwohamda@gmail.com>
2024-02-06 08:35:54 -0800

    done

--- Week 1/Day2/vite-project/src/App.jsx
+++ Week 1/Day2/vite-project/src/App.jsx
@@ -4,7 +4,9 @@ import React from "react";
 const App = ()=>{
   return <div>Hello React Amazing👍</div>,
   <h1>hellow react route</h1>,
-  <h2>hellow jijiga</h2>
+  <h2>hellow jijiga</h2>,
+  <h3>hi</h3>
+
 }
 
 export default App;
