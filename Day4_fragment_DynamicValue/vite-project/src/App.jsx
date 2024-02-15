fda6ccdab4c8273e11fe0604f7c8b825da5f0e2c
halwohamda <halwohamda@gmail.com>
2024-02-14 20:08:11 -0800

    done

---
+++ Day4_fragment_DynamicValue/vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  const message1 = 'hello worlddd';
+  const message2 = 'helllow reactjs';
+  return (
+    <>
+     <h2>{message1}</h2>
+     <h1>{message2}</h1>
+
+    </>
+  )
+}
+
+export default App
